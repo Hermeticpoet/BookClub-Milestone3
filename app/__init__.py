76cf99a55719685546b804a84dcab7277e56062b
hermeticpoet <kevywalton@gmail.com>
2019-12-02 18:12:20 +0000

    Created models.py for auth & added User class with class method

--- app/__init__.py
+++ app/__init__.py
@@ -3,9 +3,14 @@ import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bootstrap import Bootstrap
+from flask_login import LoginManager
+from flask_bcrypt import Bcrypt
+
 
 db = SQLAlchemy()
 bootstrap = Bootstrap()
+login_manager = LoginManager()
+bcrypt = Bcrypt()
 
 
 # Create function to use our app in Dev, Test or Prod
@@ -16,6 +21,8 @@ def create_app(config_type):
     app.config.from_pyfile(configuration)
     db.init_app(app)  # Bind database to Flask app
     bootstrap.init_app(app)  # Initialize Bootstrap
+    login_manager.init_app(app)  # Initialize LoginManager
+    bcrypt.init_app(app)  # Initialize Bcrypt
 
     from app.catalog import main  # Import Blueprint
     app.register_blueprint(main)  # Register Blueprint
