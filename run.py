d31e6c5af4cb482eb36da909173d9a9147b97015
hermeticpoet <kevywalton@gmail.com>
2019-12-02 12:42:25 +0000

    Created models.py file & home.html to test pulling data from database

--- run.py
+++ run.py
@@ -1,8 +1,12 @@
-from app import create_app
+from app import create_app, db
 
 
 # We can now simply pass whatever version of the app we wish to run into the function
 if __name__ == "__main__":
     flask_app = create_app("dev")
+    with flask_app.app_context():
+        db.create_all()
     flask_app.run()
+
+
 
