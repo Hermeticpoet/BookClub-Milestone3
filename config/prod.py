9a60623c784a1cf48b096792a3222537ec1b0b10
hermeticpoet <kevywalton@gmail.com>
2019-12-09 20:41:14 +0000

    Removed  code block & created environemnt varialble for production push to Heroku

--- config/prod.py
+++ config/prod.py
@@ -1,4 +1,11 @@
+import os
+
+
+# Imported OS and changed our postgreSQL database address to an environment variable for pushing out to production
 DEBUG = False
 SECRET_KEY = "topsecret"
-SQLALCHEMY_DATABASE_URI = "postgresql://postgres:topsecret@localhost/catalogue_db"
+SQLALCHEMY_DATABASE_URI = os.environ["DATABASE_URL"]
 SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+
+
